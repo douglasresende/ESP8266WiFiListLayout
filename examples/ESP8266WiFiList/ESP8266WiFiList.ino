caa136ac7c3d5b4fb78dcaecd6c0618c32722256
Douglas Camargo <douglas@maxstudio.com.br>
2016-09-06 00:20:26 -0300

    Gooo Travis ;)

--- examples/ESP8266WiFiList/ESP8266WiFiList.ino
+++ examples/ESP8266WiFiList/ESP8266WiFiList.ino
@@ -1,12 +1,11 @@
-#include "arduino.h"
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <EEPROM.h>
 #include <ESP8266WiFiListLayout.h>
 
-const char *ssid = "ESP8266";
-const char *password = "testando";
+const char *ssid = "ESP8266-01";
+const char *password = "12345678";
 
 ESP8266WebServer server(80);
 ESP8266WiFiListLayout layout;
